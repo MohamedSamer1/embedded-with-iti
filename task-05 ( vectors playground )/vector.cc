454237b49a6f0770f862ad1665758279803db476
MohamedSamer1 <mohamedsamersayed@gmail.com>
2026-07-15 03:16:06 +0300

    Added some vector initialization

--- task-05 ( vectors playground )/vector.cc
+++ task-05 ( vectors playground )/vector.cc
@@ -5,6 +5,20 @@
 
 int main()
 {
+    // 1 initialization
+    std::vector<int> v1;
 
+    // 2 initialization
+    std::vector<int> v2(5);     // 5 elements zero value
+    std::vector<int> v3(5, 10); // 5 elemets valuse = 10 {10,10,10,10,10}
+
+    // 3 initialization
+    std::vector<int> v4{1, 2};    // 2 elements value = {1,2}
+    std::vector<int> v5 = {1, 2}; // 2 elements value = {1,2}
+
+    // 4 initialization
+    int arr[] = {1, 2, 3};
+    std::vector<int> v6(arr, arr + sizeof(arr) / sizeof(arr[0]));
+    std::vector<int> v10(std::begin(arr), std::end(arr));
     return 0;
 }
